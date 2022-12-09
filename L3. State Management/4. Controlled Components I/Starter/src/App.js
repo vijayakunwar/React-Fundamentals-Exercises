737f0c9911568531e3e351515912aef8eb0feacd
vijayakunwar <kunwar.vijaya@gmail.com>
2022-12-10 00:21:57 +1000

    controlled component I - exercise - mirror text

--- L3. State Management/4. Controlled Components I/Starter/src/App.js
+++ L3. State Management/4. Controlled Components I/Starter/src/App.js
@@ -1,17 +1,31 @@
 import logo from "./logo.svg";
 import "./App.css";
+import {useState} from "react";
 
 const App = () => {
+  
+  const[text, setText] = useState("");
+
+ // manage state using setText
+
+  const handleChange = (event)=>{setText(event.target.value)};
+
+  
   return (
+    
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <h1 className="App-title">ReactND - Coding Practice</h1>
       </header>
       <div className="container">
-        <input type="text" placeholder="Say Something" />
+        <input type="text" placeholder="Say Something" value ={text} onChange ={handleChange}  />
         <p className="echo">Echo:</p>
-        <p>This should mirror the text you typed into the input field.</p>
+
+        <div>
+        <p>{text}</p>
+        </div>
+
       </div>
     </div>
   );
